33cb2d65188f9aef59ab5f2267c7d08dfcdc0a70
Esan Samuel <esansamuel555@gmail.com>
2025-12-09 22:26:51 +0100

    added suspence for usesearchparams to verify email page

--- app/(auth)/verify-email/page.tsx
+++ app/(auth)/verify-email/page.tsx
@@ -4,12 +4,12 @@ import { Loader2 } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Suspense, useEffect, useState } from "react";
 
-export default function VerifyPage() {
+function VerifyEmailContent() {
   const api = useApi();
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
   const [status, setStatus] = useState("");
-  const [verifyingEmail, setVerfyingEmail] = useState(false);
+  const [verifyingEmail, setVerifyingEmail] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
@@ -20,8 +20,8 @@ export default function VerifyPage() {
 
     const verifyEmail = async () => {
       try {
-        setVerfyingEmail(true);
-        const res = await api.post(`/api/v1/users/verify-email`, {
+        setVerifyingEmail(true);
+        const res = await api.post("/api/v1/users/verify-email", {
           token,
         });
         console.log(res);
@@ -34,24 +34,34 @@ export default function VerifyPage() {
       } catch (err) {
         setStatus("Something went wrong.");
       } finally {
-        setVerfyingEmail(false);
+        setVerifyingEmail(false);
       }
     };
 
     verifyEmail();
-  }, [token]);
+  }, [token, api, router]);
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <div className="h-screen flex items-center justify-center">
+      {verifyingEmail && (
+        <p className="flex gap-2 items-center">
+          <Loader2 className="h-5 w-5 animate-spin text-gray-400" /> Verifying
+          Email...
+        </p>
+      )}
+      {status && <p>{status}</p>}
+    </div>
+  );
+}
+
+export default function VerifyPage() {
+  return (
+    <Suspense fallback={
       <div className="h-screen flex items-center justify-center">
-        {verifyingEmail && (
-          <p className="flex gap-2 items-center">
-            <Loader2 className="h-5 w-5 animate-spin text-gray-400" /> Verifying
-            Email...
-          </p>
-        )}
-        {status}
+        <Loader2 className="h-5 w-5 animate-spin text-gray-400" />
       </div>
+    }>
+      <VerifyEmailContent />
     </Suspense>
   );
 }
