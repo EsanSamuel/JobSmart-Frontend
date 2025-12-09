3fbf2ebdb8cd5b1be44c0b8ff9ee53523af6db0d
Esan Samuel <esansamuel555@gmail.com>
2025-12-10 00:47:32 +0100

    changed endpoint from localhost to deployed endpoinyty

--- components/check-compatability.tsx
+++ components/check-compatability.tsx
@@ -73,7 +73,7 @@ const Compatibility = ({ job }: IJob) => {
     }
 
     const response = await axios.post(
-      `http://localhost:5000/api/v1/match/${job.id}`,
+      `https://jobsmart-backend.onrender.com/api/v1/match/${job.id}`,
       formData,
       {
         headers: {
